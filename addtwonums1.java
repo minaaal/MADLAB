714be1d76ace13609d60e53476c648be1f061d0e
minaaal <148136998+minaaal@users.noreply.github.com>
2026-03-28 05:20:01 +0530

    Create addtwonums1.java

---
+++ addtwonums1.java
@@ -0,0 +1,39 @@
+package com.example.skillsbuild;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText num1;
+    private EditText num2;
+    private Button add;
+    private TextView answer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = (EditText) findViewById(R.id.editTextNumber);
+        num2 = (EditText) findViewById(R.id.editTextNumber2);
+        add = (Button) findViewById(R.id.button);
+        answer = (TextView) findViewById(R.id.textView);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number1 = Integer.parseInt(num1.getText().toString());
+                int number2 = Integer.parseInt(num2.getText().toString());
+
+                int sum = number1 + number2;
+
+                answer.setText("The Answer is: " + String.valueOf(sum));
+            }
+        });
+    }
+}
